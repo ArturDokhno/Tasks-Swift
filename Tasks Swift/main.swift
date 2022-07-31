e86adfbb64b38f9dc804a31e9f2f6780ae341e63
ArturDokhno <artur.dohno@icloud.com>
2022-07-31 18:02:35 +0500

    Решение первой задачи.

--- Tasks Swift/main.swift
+++ Tasks Swift/main.swift
@@ -7,5 +7,28 @@
 
 import Foundation
 
-print("Hello, World!")
+// Задача 1: Проверка символов на уникальность
 
+// Задание: Написать функцию, которая принимает строку как свой единственный параметр и возвращает true если строка содержит только уникальные символы.
+
+// Решение:
+
+func uniquenessCheck1(input: String) -> Bool {
+    var checkedSymbols = [Character]()
+    
+    for symbol in input {
+        if checkedSymbols.contains(symbol) {
+            return false
+        }
+        checkedSymbols.append(symbol)
+    }
+    return true
+}
+
+print(uniquenessCheck1(input: "arta"))
+
+func uniquenessCheck2(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
+print(uniquenessCheck2(input: "wa"))
