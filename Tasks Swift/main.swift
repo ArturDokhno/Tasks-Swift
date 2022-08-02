3a7a575e9bbb8563cc8f23bf2a573475fa0bc00d
ArturDokhno <artur.dohno@icloud.com>
2022-08-02 18:06:08 +0500

    Решение второй задачи "Является ли строка палиндромом"

--- Tasks Swift/main.swift
+++ Tasks Swift/main.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// Задача 1: Проверка символов на уникальность
+// MARK: Задача 1: Проверка символов на уникальность
 
 // Задание: Написать функцию, которая принимает строку как свой единственный параметр и возвращает true если строка содержит только уникальные символы.
 
@@ -15,7 +15,7 @@ import Foundation
 
 func uniquenessCheck1(input: String) -> Bool {
     var checkedSymbols = [Character]()
-    
+
     for symbol in input {
         if checkedSymbols.contains(symbol) {
             return false
@@ -25,10 +25,25 @@ func uniquenessCheck1(input: String) -> Bool {
     return true
 }
 
-print(uniquenessCheck1(input: "arta"))
+//print(uniquenessCheck1(input: "Hello"))
 
 func uniquenessCheck2(input: String) -> Bool {
     return Set(input).count == input.count
 }
 
-print(uniquenessCheck2(input: "wa"))
+//print(uniquenessCheck2(input: "Hi"))
+
+
+// MARK: Задача 2: Является ли строка палиндромом ?
+
+// Задание: Написать функцию которая принимает строку и возвращает true если строка является палиндромом
+
+// Решение:
+
+func checkReversed(text: String) -> Bool {
+    let textNoSpaceAndLowercased = text.replacingOccurrences(of: " ", with: "").lowercased()
+    return String(textNoSpaceAndLowercased.reversed()) == textNoSpaceAndLowercased
+}
+
+print(checkReversed(text: "А роза упала на лапу Азора"))
+
